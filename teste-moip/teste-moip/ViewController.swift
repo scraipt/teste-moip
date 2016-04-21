99c7e0ac4cecc709c9db3cfb88aec4a43a34361f
viniciuscarvalho <viniciuscarvalho789@gmail.com>
2016-04-21 18:36:38 -0300

    commit initial

---
+++ teste-moip/teste-moip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  teste-moip
+//
+//  Created by Vinicius Carvalho on 21/04/16.
+//  Copyright © 2016 Vinicius Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
